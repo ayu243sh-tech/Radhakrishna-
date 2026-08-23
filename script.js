113647d2f7ac42c0d5a433e26d716c5cd3a9d443
ayu243sh-tech <ayu243sh@gmail.com>
2026-08-23 08:31:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,26 +3,21 @@ var autoRotate = true;
 var rotateSpeed = -60; 
 var imgWidth = 120; 
 var imgHeight = 170; 
-
 var bgMusicURL = 'https://user-images.githubusercontent.com/151072490/283747943-7b08424b-8647-4bdc-996c-965063dbb5e3.mp4';
 var bgMusicControls = true; 
-
+var minRadius = 120;
+var maxRadius = 500;
 setTimeout(init, 1000);
-
 var odrag = document.getElementById('drag-container');
 var ospin = document.getElementById('spin-container');
 var aImg = ospin.getElementsByTagName('img');
 var aVid = ospin.getElementsByTagName('video');
 var aEle = [...aImg, ...aVid];
-
-
 ospin.style.width = imgWidth + "px";
 ospin.style.height = imgHeight + "px";
-
 var ground = document.getElementById('ground');
 ground.style.width = radius * 3 + "px";
 ground.style.height = radius * 3 + "px";
-
 function init(delayTime) {
   for (var i = 0; i < aEle.length; i++) {
     aEle[i].style.transform = "rotateY(" + (i * (360 / aEle.length)) + "deg) translateZ(" + radius + "px)";
@@ -30,29 +25,23 @@ function init(delayTime) {
     aEle[i].style.transitionDelay = delayTime || (aEle.length - i) / 4 + "s";
   }
 }
-
 function applyTranform(obj) {
  
   if(tY > 180) tY = 180;
   if(tY < 0) tY = 0;
-
   obj.style.transform = "rotateX(" + (-tY) + "deg) rotateY(" + (tX) + "deg)";
 }
-
 function playSpin(yes) {
   ospin.style.animationPlayState = (yes?'running':'paused');
 }
-
 var sX, sY, nX, nY, desX = 0,
     desY = 0,
     tX = 0,
     tY = 10;
-
 if (autoRotate) {
   var animationName = (rotateSpeed > 0 ? 'spin' : 'spinRevert');
   ospin.style.animation = `${animationName} ${Math.abs(rotateSpeed)}s infinite linear`;
 }
-
 if (bgMusicURL) {
   document.getElementById('music-container').innerHTML += `
 <audio src="${bgMusicURL}" ${bgMusicControls? 'controls': ''} autoplay loop>    
@@ -60,13 +49,15 @@ if (bgMusicURL) {
 </audio>
 `;
 }
-
 document.onpointerdown = function (e) {
-  clearInterval(odrag.timer);
   e = e || window.event;
+  // don't hijack rotation when the touch/drag starts on the zoom slider
+  if (e.target.closest && e.target.closest('#zoom-container')) {
+    return;
+  }
+  clearInterval(odrag.timer);
   var sX = e.clientX,
       sY = e.clientY;
-
   this.onpointermove = function (e) {
     e = e || window.event;
     var nX = e.clientX,
@@ -79,7 +70,6 @@ document.onpointerdown = function (e) {
     sX = nX;
     sY = nY;
   };
-
   this.onpointerup = function (e) {
     odrag.timer = setInterval(function () {
       desX *= 0.95;
@@ -95,13 +85,31 @@ document.onpointerdown = function (e) {
     }, 17);
     this.onpointermove = this.onpointerup = null;
   };
-
   return false;
 };
-
 document.onmousewheel = function(e) {
   e = e || window.event;
   var d = e.wheelDelta / 20 || -e.detail;
   radius += d;
+  if (radius < minRadius) radius = minRadius;
+  if (radius > maxRadius) radius = maxRadius;
   init(1);
+  syncZoomSlider();
 };
+
+// ---------- zoom dragger (mobile) ----------
+var zoomSlider = document.getElementById('zoom-slider');
+if (zoomSlider) {
+  zoomSlider.value = radius;
+  zoomSlider.addEventListener('input', function () {
+    radius = Number(this.value);
+    init(1);
+  });
+  // stop the slider's own drag from bubbling into the carousel rotate handler
+  zoomSlider.addEventListener('pointerdown', function (e) {
+    e.stopPropagation();
+  });
+}
+function syncZoomSlider() {
+  if (zoomSlider) zoomSlider.value = radius;
+}
